278aaf9e4ca9bb728dfd050c6d9d9e66cab94e05
Areebey <areebrafiq125@gmail.com>
2023-10-20 04:06:24 +0500

    initialize chapter 6-9

---
+++ Chapters/Chapter no 6-9/App_maths_Expressions_n_others.js
@@ -0,0 +1,5 @@
+// Chapter no 6 to 9 : Maths Expressions
+
+// Qno1. Write a program to take a number in a variable, do the required 
+// arithmetic to display the following result in your browser:  
+ 
